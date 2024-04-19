5cf6ab93c2d42cbd603f04ffbdd48394b383c0b9
Novo1999 <neptune.navo@gmail.com>
2024-04-20 01:10:26 +0600

    comment

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,5 +1,5 @@
-import { getBooks } from '@/lib/book-data'
-import Books from './Books'
+// import { getBooks } from '@/lib/book-data'
+// import Books from './Books'
 import SearchInput from './SearchInput'
 
 const Library = async () => {
@@ -9,7 +9,7 @@ const Library = async () => {
     <div className='h-96 mt-6 p-2 flex flex-col overflow-auto gap-2 w-[23rem] rounded-md bg-green-300'>
       <h1 className='border-b-black border-b font-semibold'>Books</h1>
       <SearchInput />
-      <Books books={books} />
+      {/* <Books books={books} /> */}
     </div>
   )
 }
